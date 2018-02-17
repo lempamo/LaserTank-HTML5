994fe753a36375db5083f9d7e6d727a805add3b3
lempamo <lempamo@outlook.com>
2018-02-16 20:12:11 -0500

    all the sounds preloaded

--- src/preload.js
+++ src/preload.js
@@ -10,8 +10,19 @@ preload.prototype = {
         this.game.load.audio("snd-break-anti", "/assets/sounds/break-anti.wav");
         this.game.load.audio("snd-break-bricks", "/assets/sounds/break-bricks.wav");
         this.game.load.audio("snd-die", "/assets/sounds/die.wav");
+        this.game.load.audio("snd-level-end", "/assets/sounds/level-end.wav");
+        this.game.load.audio("snd-move", "/assets/sounds/move.wav");
+        this.game.load.audio("snd-push-anti", "/assets/sounds/push-anti.wav");
+        this.game.load.audio("snd-push-block", "/assets/sounds/push-block.wav");
+        this.game.load.audio("snd-push-block", "/assets/sounds/push-reflector.wav");
         this.game.load.audio("snd-reflect", "/assets/sounds/reflect.wav");
         this.game.load.audio("snd-shoot-anti", "/assets/sounds/shoot-anti.wav");
+        this.game.load.audio("snd-shoot-tank", "/assets/sounds/shoot-tank.wav");
+        this.game.load.audio("snd-sink", "/assets/sounds/sink.wav");
+        this.game.load.audio("snd-turn-reflector", "/assets/sounds/turn-reflector.wav");
+        this.game.load.audio("snd-turn-tank", "/assets/sounds/turn-tank.wav");
+        this.game.load.audio("snd-wallbump-tank", "/assets/sounds/wallbump-tank.wav");
+        this.game.load.audio("snd-wallbump-laser", "/assets/sounds/wallbump-laser.wav");
 
         this.game.load.binary("levels-regular", "/assets/LaserTank.lvl");
     }
