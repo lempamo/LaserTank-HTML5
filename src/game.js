72b5cd023301f42edb52178f7a848bf2b57b9715
lempamo <lempamo@outlook.com>
2018-02-17 18:23:24 -0500

    quick-fix

--- src/game.js
+++ src/game.js
@@ -13,8 +13,8 @@ ltgame.prototype = {
     create: function(){
         var bg = this.add.sprite(0, 0, "bg");
         g_splash = this.add.group();
-        var s_splash = g_splash.add.sprite(17, 17, "startscreen");
-        var g01 = g_splash.add.sprite(20, 209, "tiles24", 0);
+        var s_splash = this.add.sprite(17, 17, "startscreen", 0, g_splash);
+        var g01 = this.add.sprite(20, 209, "tiles24", 0, g_splash);
     },
     update: function(){
 
