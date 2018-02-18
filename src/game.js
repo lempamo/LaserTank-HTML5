804771af64c105909294d497c8ddfd681e1d3825
lempamo <lempamo@outlook.com>
2018-02-18 11:20:16 -0500

    splash screen done

--- src/game.js
+++ src/game.js
@@ -14,8 +14,27 @@ ltgame.prototype = {
         var bg = this.add.sprite(0, 0, "bg");
         g_splash = this.add.group();
         var s_splash = this.add.sprite(17, 17, "startscreen", 0, g_splash);
-        var g01 = this.add.sprite(20, 209, "tiles24", 0, g_splash);
-        var g02 = this.add.sprite(20, 209, "tiles24", 3, g_splash);
+        this.add.sprite(20, 209, "tiles24", 0, g_splash);
+        this.add.sprite(20, 209, "tiles24", 3, g_splash);
+        this.add.sprite(116, 209, "tiles24", 5, g_splash);
+        this.add.sprite(212, 209, "tiles24", 0, g_splash);
+        this.add.sprite(308, 209, "tiles24", 8, g_splash);
+        this.add.sprite(20, 257, "tiles24", 55, g_splash);
+        this.add.sprite(116, 257, "tiles24", 56, g_splash);
+        this.add.sprite(212, 257, "tiles24", 32, g_splash);
+        this.add.sprite(308, 257, "tiles24", 18, g_splash);
+        this.add.sprite(20, 305, "tiles24", 0, g_splash);
+        this.add.sprite(20, 305, "tiles24", 15, g_splash);
+        this.add.sprite(116, 305, "tiles24", 12, g_splash);
+        this.add.sprite(212, 305, "tiles24", 0, g_splash);
+        this.add.sprite(212, 305, "tiles24", 13, g_splash);
+        this.add.sprite(308, 305, "tiles24", 44, g_splash);
+        this.add.sprite(20, 353, "tiles24", 14, g_splash);
+        this.add.sprite(116, 353, "tiles24", 68, g_splash);
+        this.add.sprite(212, 353, "tiles24", 0, g_splash);
+        this.add.sprite(212, 353, "tiles24", 20, g_splash);
+        this.add.sprite(308, 353, "tiles24", 46, g_splash);
+
     },
     update: function(){
 
